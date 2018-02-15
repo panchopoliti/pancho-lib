8d5a31713098ee73abe4e7a9d98d0cd835de9cca
Francisco Politi <fran@alenta.me>
2018-02-15 15:47:56 -0300

    correcting toggle-attribute

--- libs/toggle-attribute.js
+++ libs/toggle-attribute.js
@@ -1,26 +1,20 @@
-(function (root) {
+(function () {
   function toggleElement() {
     const allToggleTriggers = document.querySelectorAll('[data-toggle]');
 
     for (const trigger of allToggleTriggers) {
-      const targetString = trigger.getAttribute('[data-target]');
+      const targetString = trigger.getAttribute('data-target');
       const target = document.getElementById(targetString);
       trigger.addEventListener('click', () => {
         target.classList.toggle('hide');
       });
 
       document.body.addEventListener('click', (event) => {
-
         if (event.target !== trigger) {
           target.classList.add('hide');
         }
-
       });
     }
   }
   toggleElement();
-
-  root.toggle = {
-    toggleElement,
-  }
-}(window));
+}());
