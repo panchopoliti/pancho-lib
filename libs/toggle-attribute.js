aab2ffcb336d44619f036c4798315f4e1163425b
Francisco Politi <fran@alenta.me>
2018-02-16 11:10:22 -0300

    Improving toggle function

--- libs/toggle-attribute.js
+++ libs/toggle-attribute.js
@@ -1,23 +1,39 @@
 (function (root) {
-  function toggleElement() {
+
+  function onlyToggle(trigger, target) {
+    trigger.addEventListener('click', () => {
+      target.classList.toggle('hide');
+    });
+  }
+
+  function clickOutside(trigger, target) {
+    document.body.addEventListener('click', (event) => {
+      if (event.target !== trigger) {
+        target.classList.add('hide');
+      }
+    });
+  }
+
+  function genericToggle() {
     const allToggleTriggers = document.querySelectorAll('[data-toggle]');
 
     for (const trigger of allToggleTriggers) {
+      const toggleValue = trigger.dataset.toggle;
       const targetString = trigger.getAttribute('data-target');
       const target = document.getElementById(targetString);
-      trigger.addEventListener('click', () => {
-        target.classList.toggle('hide');
-      });
 
-      document.body.addEventListener('click', (event) => {
-        if (event.target !== trigger) {
-          target.classList.add('hide');
-        }
-      });
+      if (toggleValue === 'onlyToggle') {
+        onlyToggle(trigger, target);
+      } else if (toggleValue === 'clickOutside') {
+        onlyToggle(trigger, target);
+        clickOutside(trigger, target);
+      } else {
+        throw new Error(`${toggleValue} is not an available data-toggle value`);
+      }
     }
   }
-  toggleElement();
+
   root.toggle = {
-    toggleElement,
+    genericToggle,
   };
 }(window));
